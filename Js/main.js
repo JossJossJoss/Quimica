5cb7ac3e943eb90f0727379ca193b19e1ee2863c
JossJossJoss <Joszedz@gmail.com>
2014-06-18 15:48:52 -0500

    nw

---
+++ Js/main.js
@@ -0,0 +1,23 @@
+var myName = "Jouna Gay";
+
+
+var red = [80, 120, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [155, 80, 70];
+var purple = [280, 50, 60];
+var letterColors = [red, orange, green, blue, purple];
+
+drawName(myName, letterColors);
+
+
+if(10 < 3)
+{
+    bubbleShape = 'square';
+}
+else
+{
+    bubbleShape = 'circle';
+}
+
+bounceBubbles();

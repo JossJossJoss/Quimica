aeddc1482f2528b51f351e5e3f85484724e9d429
JossJossJoss <Joszedz@gmail.com>
2014-06-22 18:59:25 -0500

    V 3.0

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,11 @@
-var myName = "Jouna Gay";
+var myName = "ELEMENTALY";
 
 
-var red = [80, 120, 63];
+var red = [8, 190, 60];
 var orange = [40, 100, 60];
-var green = [75, 100, 40];
+var green = [80, 120, 63];
 var blue = [155, 80, 70];
-var purple = [280, 50, 60];
+var purple = [280, 50, 40];
 var letterColors = [red, orange, green, blue, purple];
 
 drawName(myName, letterColors);
